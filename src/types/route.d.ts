27f975ead640cdefb449ebc6fd180d123644eaac
F999999999 <9811@88.com>
2022-06-09 22:36:13 +0800

    Update

--- src/types/route.d.ts
+++ src/types/route.d.ts
@@ -1,11 +1,7 @@
 import { RouteObject } from "react-router-dom";
 
 export interface RouterBody extends RouteObject {
-  name?: string;
-  path: string;
-  component?: any;
-  element?: any;
   children?: RouterBody[];
-  auth?: boolean;
   title?: string;
+  auth?: boolean;
 }
