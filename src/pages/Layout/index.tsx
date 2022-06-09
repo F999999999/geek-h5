bbdf231fc76bc0a51c245878223febb760cacc57
F999999999 <9811@88.com>
2022-06-09 22:28:11 +0800

    完成：登录页面布局

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,17 +1,5 @@
-import { Button } from "antd-mobile";
-import Icon from "@/components/Icon";
-
 const Layout = () => {
-  return (
-    <div>
-      Layout
-      <Button color="primary" fill="solid">
-        Solid
-      </Button>
-      <Button>Button</Button>
-      <Icon type={"iconbtn_like_sel"} />
-    </div>
-  );
+  return <div>首页</div>;
 };
 
 export default Layout;
