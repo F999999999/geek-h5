dabfcf6a516c3630b9e721cfe9f607b673f079f3
F999999999 <9811@88.com>
2022-06-09 10:12:27 +0800

    优化：antd-mobile 国际化配置

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
+import { ConfigProvider } from "antd-mobile";
+import zhCN from "antd-mobile/es/locales/zh-CN";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>
+);
