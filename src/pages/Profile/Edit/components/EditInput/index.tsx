6163cad9f384808fd6664732ca38b556ac90e019
F999999999 <9811@88.com>
2022-06-14 10:11:01 +0800

    Update

--- src/pages/Profile/Edit/components/EditInput/index.tsx
+++ src/pages/Profile/Edit/components/EditInput/index.tsx
@@ -1,5 +1,5 @@
 import {Input, NavBar, TextArea} from "antd-mobile";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 import styles from "./index.module.scss";
 
@@ -18,15 +18,9 @@ const EditInput = ({ onClose, value, onUpdateProfile, type }: Props) => {
   // 这样操作后，进入该页面，文本框的值就是父组件中传递过来的 value 值了
   const [inputValue, setInputValue] = useState(value);
 
-  // 解决无法正常展示简介信息的Bug
-  useEffect(() => {
-    setInputValue(value);
-  }, [value]);
-
   // 创建变量，用来表示是否为修改昵称
   const isEditName = type === "name";
 
-  // 提交修改
   const onSave = () => {
     if (type !== "") onUpdateProfile(type, inputValue);
   };
