2f2dd7dae9cfe85d1d892e4d159ac72abcc638f2
F999999999 <9811@88.com>
2022-06-17 22:08:38 +0800

    完成：文章详情-导航栏展示作者信息

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,22 +1,27 @@
 import styles from "./index.module.scss";
-import { useParams } from "react-router-dom";
-
-import { useEffect, useRef } from "react";
-import { useAppDispatch, useAppSelector } from "@/store";
-import { ARTICLE_FEATURE_KEY, getArticle } from "@/store/articleSlice";
-import classNames from "classnames";
 
 import dayjs from "dayjs";
 // 导入本地化格式插件
 import localizedFormat from "dayjs/plugin/localizedFormat";
 // 导入对 HTML 格式字符串消毒的包
 import DOMPurify from "dompurify";
+// 导入 lodash 的节流函数
+import { throttle } from "lodash";
+
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { ARTICLE_FEATURE_KEY, getArticle } from "@/store/articleSlice";
+import classNames from "classnames";
+import { NavBar } from "antd-mobile";
+import Icon from "@/components/Icon";
 
 dayjs.extend(localizedFormat);
 
 const Article = () => {
   const params = useParams();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const {
     articleDetail: {
       content,
@@ -37,10 +42,37 @@ const Article = () => {
   const authorRef = useRef<HTMLDivElement>(null);
   // 评论信息的 DOM 对象的 ref
   const commentRef = useRef<HTMLDivElement>(null);
+  // 文章是否加载中的状态
+  const [loading, setLoading] = useState(true);
+  // 导航栏中作者信息是否展示
+  const [isShowNavAuthor, setIsShowNavAuthor] = useState(false);
 
+  // 文章数据
   useEffect(() => {
     params.id && dispatch(getArticle(params.id));
+    setLoading(false);
   }, [dispatch, params.id]);
+
+  // 导航栏中展示作者信息
+  useEffect(() => {
+    if (loading) return;
+
+    const wrapperDOM = wrapperRef.current!;
+
+    // 创建一个节流函数
+    const handleScroll = throttle(() => {
+      const { bottom } = authorRef.current!.getBoundingClientRect();
+      // 44 是 NavBar 的高度，因为 NavBar 会挡住页面内容，所以，此处需要减去它的高度
+      if (bottom - 44 <= 0) {
+        setIsShowNavAuthor(true);
+      } else {
+        setIsShowNavAuthor(false);
+      }
+    }, 200);
+
+    wrapperDOM.addEventListener("scroll", handleScroll);
+    return () => wrapperDOM.removeEventListener("scroll", handleScroll);
+  }, [loading]);
 
   // 渲染文章详情
   const renderArticle = () => {
@@ -97,7 +129,40 @@ const Article = () => {
     );
   };
 
-  return <div className={styles.root}>{renderArticle()}</div>;
+  return (
+    <div className={styles.root}>
+      <div className="root-wrapper">
+        {/* 导航栏 */}
+        <NavBar
+          onBack={() => navigate(-1)}
+          right={
+            <span>
+              <Icon type="icongengduo" />
+            </span>
+          }
+        >
+          {isShowNavAuthor && (
+            <div className="nav-author">
+              <img
+                src={
+                  aut_photo || "http://geek.itheima.net/images/user_head.jpg"
+                }
+                alt=""
+              />
+              <span className="name">{aut_name}</span>
+              <span
+                className={classNames("follow", is_followed ? "followed" : "")}
+              >
+                {is_followed ? "已关注" : "关注"}
+              </span>
+            </div>
+          )}
+        </NavBar>
+        {/* 文章详情 */}
+        {renderArticle()}
+      </div>
+    </div>
+  );
 };
 
 export default Article;
